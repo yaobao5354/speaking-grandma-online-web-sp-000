049a5d8dbcdde5fef4fb6b573492a4a6c48550e2
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 17:53:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,10 @@
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  
+  elsif speak.downcase == speak.swapcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 
 end
