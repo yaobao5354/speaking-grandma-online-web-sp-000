d36410b030b276345686f4ae30a543cb43144f1e
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 17:49:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(speak)
-  if speak == "I LOVE YOU GRANDMA"
+  if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   
   end
